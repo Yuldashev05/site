8baf1b176686fa5cc6d50d2a99e55ccd62d3e898
Yuldashev05 <akpuska73@gmail.com>
2024-02-13 02:07:38 +0500

    Blog

---
+++ frontend/src/components/Blog/Calendar.jsx
@@ -0,0 +1,27 @@
+ 
+ 
+function Calendar() {
+     return ( 
+        <>
+            <div className="calendar">
+        <table>
+          <thead>
+            <tr>
+              <th>Sun</th>
+              <th>Mon</th>
+              <th>Tue</th>
+              <th>Wed</th>
+              <th>Thu</th>
+              <th>Fri</th>
+              <th>Sat</th>
+            </tr>
+          </thead>
+          <tbody>
+          </tbody>
+        </table>
+            </div>
+        </>
+      );
+}
+
+export default Calendar;
